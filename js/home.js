1d68903b964364b3439a1ea31ded720aa561aed8
ThomasGRG <kurianthomasjunior@gmail.com>
2020-11-29 14:56:29 +0530

    Add horizontal scrolling cards

---
+++ js/home.js
@@ -0,0 +1,30 @@
+$( document ).ready(function() {
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:5,
+        stagePadding: 50,
+        autoplay:true,
+        autoplayTimeout:4000,
+        autoplayHoverPause:true,
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:5
+            }
+        }
+    });
+    $('.owl-carousel').on('mousewheel', '.owl-stage', function (e) {
+        e.preventDefault();
+        if (e.deltaY>0) {
+            $('.owl-carousel').trigger('next.owl');
+        } else {
+            $('.owl-carousel').trigger('prev.owl');
+        }
+    });
+});
