c398a1f322fabb876e6341eadd36ba6b587e94fa
ThomasGRG <kurianthomasjunior@gmail.com>
2020-11-30 07:41:57 +0530

    Add cart page and date range picker to item page

--- js/item.js
+++ js/item.js
@@ -1,32 +1,20 @@
+var picker = new Litepicker({
+    element: document.getElementById('litepicker'),
+    firstDay:1,
+    format:'D MMM, YYYY',
+    lang:'en-US',
+    numberOfMonths:2,
+    numberOfColumns:2,
+    splitView:true,
+    inlineMode:true,
+    singleMode:false,
+    autoApply:true,
+    showTooltip:true,
+    useResetBtn:true,
+    mobileFriendly:true,
+});
+
 $( document ).ready(function() {
-    $('.owl-carousel').owlCarousel({
-        loop:true,
-        margin:5,
-        stagePadding: 50,
-        autoplay:true,
-        autoplayTimeout:4000,
-        autoplayHoverPause:true,
-        nav:true,
-        responsive:{
-            0:{
-                items:1
-            },
-            600:{
-                items:3
-            },
-            1000:{
-                items:5
-            }
-        }
-    });
-    $('.owl-carousel').on('mousewheel', '.owl-stage', function (e) {
-        e.preventDefault();
-        if (e.deltaY>0) {
-            $('.owl-carousel').trigger('next.owl');
-        } else {
-            $('.owl-carousel').trigger('prev.owl');
-        }
-    });
     $(window).scroll(function(){
         if($(this).scrollTop() > 250){
             $('.myBtn').fadeIn();
